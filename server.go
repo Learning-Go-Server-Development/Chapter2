1e3221e9a8186541105f9adb77e6fe0537ecebbc
KenWilliamson <kenulborapublic@gmail.com>
2026-01-24 17:48:59 -0500

    update for DeliveryOrder

--- server.go
+++ server.go
@@ -7,7 +7,98 @@ import (
 	"time"
 )
 
+type Customer struct {
+	Name           string
+	Address        string
+	CustomerNumber string
+	Phone          string
+}
+
+type StoreOrder interface {
+	CreateOrder(cid string, sku string)
+	UpdateOrder(sku string)
+	Print(label string)
+}
+
+type Order struct {
+	CustomerNumber string
+	ItemSku        string
+	TimeCreated    time.Time
+	UpdatedTime    time.Time
+}
+
+func (o *Order) CreateOrder(cid string, sku string) {
+	o.CustomerNumber = cid
+	o.ItemSku = sku
+	o.TimeCreated = time.Now()
+}
+
+func (o *Order) UpdateOrder(sku string) {
+	o.ItemSku = sku
+	o.UpdatedTime = time.Now()
+}
+
+func (o *Order) Print(label string) {
+	fmt.Println(label, o)
+}
+
+func (o *Order) New() StoreOrder {
+	return o
+}
+
+type DeliverOrder struct {
+	CustomerNumber string
+	ItemSku        string
+	TimeCreated    time.Time
+	UpdatedTime    time.Time
+	DeliveryTime   time.Time
+}
+
+func (o *DeliverOrder) CreateOrder(cid string, sku string) {
+	o.CustomerNumber = cid
+	o.ItemSku = sku
+	o.TimeCreated = time.Now()
+}
+
+func (o *DeliverOrder) UpdateOrder(sku string) {
+	o.ItemSku = sku
+	o.UpdatedTime = time.Now()
+	o.DeliveryTime = time.Now().Add(time.Hour * 24 * 14)
+}
+
+func (o *DeliverOrder) Print(label string) {
+	fmt.Println("This is a deliver order and will be delivered in two weeks or less "+label, o)
+	if !o.DeliveryTime.IsZero() {
+		fmt.Println("Delivery Date: ", o.DeliveryTime)
+	}
+}
+
+func (o *DeliverOrder) New() StoreOrder {
+	return o
+}
+
 func main() {
+
+	// moved to an interface below
+	// var odr Order
+	// odr.CreateOrder("12345", "sku123")
+	// fmt.Println("new order: ", odr)
+
+	var odr Order
+	so := odr.New()
+	so.CreateOrder("12345", "sku123")
+	so.Print("new order: ")
+
+	so.UpdateOrder("sku123---part 2")
+	so.Print("updated order: ")
+
+	var dodr DeliverOrder
+	dso := dodr.New()
+	dso.CreateOrder("12345", "sku123888")
+	dso.Print("(new order): ")
+
+	dso.UpdateOrder("sku123888---part 2")
+	dso.Print("(updated order): ")
 
 	var wg sync.WaitGroup
 
